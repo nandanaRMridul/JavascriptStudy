9a8e159d61330a3c6d8b8acad33eeba83330aaf2
Nandana R <nandana.rm99@gmail.com>
2025-03-22 09:12:44 +0530

    Added practice problems

--- PRACTICE/ObjectPractice/3.js
+++ PRACTICE/ObjectPractice/3.js
@@ -3,9 +3,26 @@ const itemList = [
   { item: "Carrot", category: "Vegetable" },
   { item: "Banana", category: "Fruit" },
   { item: "Broccoli", category: "Vegetable" },
-  { item: "Orange", category: "Fruit" },
+  { item: "Orange", category: "Fruit" }
 ];
 
 // Create an object where the keys are the categories, and the values are arrays of items in each category.
 // Expected output example: { "Fruit": ["Apple", "Banana", "Orange"], "Vegetable": ["Carrot", "Broccoli"] }
 // Write the javascript code to fulfill the requirement.
+let output = {};
+
+itemList.forEach((eachItem) => {
+  category = eachItem.category;
+  currentItem = eachItem.item;
+  items = [];
+
+  if (category in output) {
+    output[category].push(currentItem);
+  }
+  else {
+    output[category] = [currentItem];
+  }
+})
+
+console.log(output);
+
